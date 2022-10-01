61a54a659e42b97c7583dee10bd4b6ddd8b59eef
Allan Misasa Nielsen <amni1@ucl.dk>
2022-10-01 12:58:05 +0000

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,10 +2,10 @@ module.exports = {
   docs: [
     {
       type: 'category',
-      label: 'Docusaurus Tutorial',
+      label: 'Guides',
       items: [
-        'getting-started',
-        'create-a-page',
+        'flashing-micropython-on-esp32',
+        'oop',
         'create-a-document',
         'create-a-blog-post',
         'markdown-features',
